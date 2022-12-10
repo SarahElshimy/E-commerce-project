4575d3fa7006b4b6a49cae0745b5e8ce4cfedab5
sarahelshimy <sarahelshimy18@gmail.com>
2022-12-10 20:45:21 +0200

    Online Shop project

---
+++ E-CommerceAutomation-main/src/main/java/OnlineShop/angularAutomation/strategy/testData/TestDataStrategy.java
@@ -0,0 +1,12 @@
+package OnlineShop.angularAutomation.strategy.testData;
+
+import java.util.ArrayList;
+
+
+public interface TestDataStrategy {
+
+	ArrayList<ArrayList<Object>> loadTestData(String connectionProperties);
+
+	Object[][] getTestDataFromExtSource(String connectionProperties, Class dataModelClass);
+
+}
